5708923884b9555b0bf7d7ed521422cedff7c50c
Chris Elsworth <chris@cae.me.uk>
2019-09-05 16:45:34 +0100

    better way of representing msb

--- lib/lxp/packet.rb
+++ lib/lxp/packet.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'packet/registers'
+require_relative 'packet/register_bits'
 
 require_relative 'packet/read_single'
 require_relative 'packet/write_single'
--- lib/lxp/packet/register_bits.rb
+++ lib/lxp/packet/register_bits.rb
@@ -4,8 +4,18 @@ class LXP
   class Packet
     module RegisterBits
       ###
-      ### Register 21, Least Significant Byte
+      ### Register 21, Most Significant Byte
       ###
+      FEED_IN_GRID            = 1 << 15
+      DCI_ENABLE              = 1 << 14
+      GFCI_ENABLE             = 1 << 13
+      R21_UNKNOWN_BIT_12      = 1 << 12
+      CHARGE_PRIORITY         = 1 << 11
+      FORCED_DISCHARGE_ENABLE = 1 << 10
+      NORMAL_OR_STANDBY       = 1 << 9
+      SEAMLESS_EPS_SWITCHING  = 1 << 8
+
+      ### Register 21, Least Significant Byte
       AC_CHARGE_ENABLE       = 1 << 7
       GRID_ON_POWER_SS       = 1 << 6
       NEUTRAL_DETECT_ENABLE  = 1 << 5
@@ -14,16 +24,14 @@ class LXP
       OVF_LOAD_DERATE_ENABLE = 1 << 1
       POWER_BACKUP_ENABLE    = 1 << 0
 
-      ###
-      ### Register 21, Most Significant Byte
-      ###
-      FEED_IN_GRID            = 1 << 7
-      DCI_ENABLE              = 1 << 6
-      GFCI_ENABLE             = 1 << 5
-      CHARGE_PRIORITY         = 1 << 3
-      FORCED_DISCHARGE_ENABLE = 1 << 2
-      NORMAL_OR_STANDBY       = 1 << 1
-      SEAMLESS_EPS_SWITCHIGN  = 1 << 0
+      # Not a recommendation, just what my defaults appeared to be when
+      # setting up the unit for the first time, so probably sane..?
+      R21_DEFAULTS = FEED_IN_GRID |
+                     DCI_ENABLE | GFCI_ENABLE |
+                     R21_UNKNOWN_BIT_12 |
+                     NORMAL_OR_STANDBY | SEAMLESS_EPS_SWITCHING |
+                     GRID_ON_POWER_SS | ANTI_ISLAND_ENABLE |
+                     DRMS_ENABLE
 
       ###
       ### Register 105, Least Significant Byte
